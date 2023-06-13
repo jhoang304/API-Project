04dd5dbf912c37329916f04b907c91f567f13a2a
Joshua <jhoang304@gmail.com>
2023-06-13 10:44:37 -0500

    get all spots owned by current user completed

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -4,31 +4,67 @@ const { Op } = require('sequelize')
 const { requireAuth } = require('../../utils/auth');
 const { User, Spot, SpotImage, Review, ReviewImage, Booking } = require('../../db/models');
 
-// Main Function
-router.get('/', async (req, res) => {
+//Get all Spots owned by Current User
+router.get('/current', requireAuth, async (req, res) => {
+  try {
+    const id = req.user.id;
     const spots = await Spot.findAll({
-        include: [
-          { model: Review },
-          { model: SpotImage }
-        ]
-      });
+      where: { ownerId: id },
+      include: [
+        { model: Review },
+        { model: SpotImage }
+      ]
+    });
 
-      const spotsList = spots.map(spot => {
-        const spotData = spot.toJSON();
-        const sum = spotData.Reviews.reduce((total, review) => total + review.stars, 0);
-        spotData.avgRating = sum / spotData.Reviews.length || 0;
+    const spotsList = spots.map(spot => {
+      const spotData = spot.toJSON();
+      const sum = spotData.Reviews.reduce((total, review) => total + review.stars, 0);
+      spotData.avgRating = sum / spotData.Reviews.length;
+      delete spotData.Reviews;
 
-        const spotImage = spotData.SpotImages.find(image => image.url);
-        spotData.previewImage = spotImage ? spotImage.url : 'no image found';
+      spotData.previewImage = 'none';
+      const prevImg = spotData.SpotImages.find(el => el.preview === true);
+      if (prevImg) {
+        spotData.previewImage = prevImg.url;
+      }
+      delete spotData.SpotImages;
 
-        delete spotData.Reviews;
-        delete spotData.SpotImages;
+      return spotData;
+    });
 
-        return spotData;
-      });
+    const result = { Spots: spotsList };
+    return res.json(result);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
-      const result = { Spots: spotsList };
-      return res.json(result);
+// Get All Spots
+router.get('/', async (req, res) => {
+  const spots = await Spot.findAll({
+    include: [
+      { model: Review },
+      { model: SpotImage }
+      ]
+  });
+
+  const spotsList = spots.map(spot => {
+    const spotData = spot.toJSON();
+    const sum = spotData.Reviews.reduce((total, review) => total + review.stars, 0);
+    spotData.avgRating = sum / spotData.Reviews.length || 0;
+
+    const spotImage = spotData.SpotImages.find(image => image.url);
+    spotData.previewImage = spotImage ? spotImage.url : 'no image found';
+
+    delete spotData.Reviews;
+    delete spotData.SpotImages;
+
+    return spotData;
+  });
+
+  const result = { Spots: spotsList };
+  return res.json(result);
 });
 
 
