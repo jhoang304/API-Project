1d0e7b0c4187000b56632cc6b65a8061fd8e3572
Joshua <jhoang304@gmail.com>
2023-06-13 11:48:52 -0500

    get details for a spot from an id

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -40,6 +40,52 @@ router.get('/current', requireAuth, async (req, res) => {
   }
 });
 
+// Get details of a Spot from an id
+router.get('/:spotId', async (req, res) => {
+  try {
+    const { spotId } = req.params;
+    const item = await Spot.findOne({
+      where: { id: spotId },
+      include: [
+        { model: Review },
+        {
+          model: SpotImage,
+          attributes: {
+            exclude: ['spotId', 'createdAt', 'updatedAt']
+          }
+        }
+      ]
+    });
+
+    if (!item) {
+      return res.status(404).json({
+        message: "Spot couldn't be found"
+      });
+    }
+
+    let spot = item.toJSON();
+
+    spot.numReviews = spot.Reviews.length;
+
+    const sum = spot.Reviews.reduce((total, review) => total + review.stars, 0);
+    spot.avgStarRating = sum / spot.Reviews.length;
+    delete spot.Reviews;
+
+    const imgHolder = spot.SpotImages;
+    delete spot.SpotImages;
+    spot.SpotImages = imgHolder;
+
+    spot.Owner = await User.findByPk(spot.ownerId, {
+      attributes: ['id', 'firstName', 'lastName']
+    });
+
+    return res.json(spot);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
 // Get All Spots
 router.get('/', async (req, res) => {
   const spots = await Spot.findAll({
