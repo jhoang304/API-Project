8094b3d7d3edb4138c8a1a52f308cec6246d2eba
Joshua <jhoang304@gmail.com>
2023-06-15 16:26:16 -0500

    delete a review image

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -6,6 +6,7 @@ const spotsRouter = require('./spots.js');
 const reviewsRouter = require('./reviews.js');
 const bookingsRouter = require('./bookings.js');
 const spotImagesRouter = require('./spot-images.js');
+const reviewImagesRouter = require('./review-images.js');
 
 const { restoreUser } = require("../../utils/auth.js");
 
@@ -20,6 +21,7 @@ router.use('/spots', spotsRouter);
 router.use('/reviews', reviewsRouter);
 router.use('/bookings', bookingsRouter);
 router.use('/spot-images', spotImagesRouter);
+router.use('/review-images', reviewImagesRouter);
 
 
 module.exports = router;
--- backend/routes/api/review-images.js
+++ backend/routes/api/review-images.js
@@ -5,25 +5,25 @@ const { requireAuth } = require('../../utils/auth');
 
 const { User, Spot, SpotImage, Review, ReviewImage, Booking } = require('../../db/models');
 
-// Delete an Image for a Spot
+// Delete a Review Image
 router.delete('/:imageId', requireAuth, async (req, res) => {
     const { imageId } = req.params;
     const user = req.user.id;
-    const image = await SpotImage.findByPk(imageId);
+    const image = await ReviewImage.findByPk(imageId);
 
     if(!image) {
         res.status(404);
-        return res.json({ message: "Spot Image couldn't be found" });
+        return res.json({ message: "Review Image couldn't be found" });
     }
 
-    const spot = await Spot.findByPk(image.spotId);
-    if(user !== spot.ownerId) {
+    const review = await Review.findByPk(image.reviewId);
+    if(user !== review.userId) {
         res.status(403);
-        return res.json({ message: "Only the user can delete the image of their spot" });
-    }
+        return res.json({ message: "Only the user can delete the image of their review" });
+    };
 
     await image.destroy();
     return res.json({ message: "successfully deleted" });
-})
+});
 
 module.exports = router;
